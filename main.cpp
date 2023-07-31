c5d3f0a6714cc5193490b3720f6f45c381043fbe
sledge79 <tonywalsh1511@gmail.com>
2023-07-31 16:35:53 +0000

    Final Commit
    Signed-off-by: sledge79 <tonywalsh1511@gmail.com>

--- main.cpp
+++ main.cpp
@@ -11,14 +11,9 @@ static LSM6DSLSensor acc_gyro(&devI2c,0xD4,D4,D5); // high address
 float compute (int x, int y, int z, int x_ref, int y_ref, int z_ref) {
     
     float dot_product = x * x_ref + y * y_ref + z * z_ref;
-
-   
     float magnitude1 = sqrt(x * x + y * y + z * z);
     float magnitude2 = sqrt(x_ref * x_ref + y_ref * y_ref + z_ref * z_ref);
-    
     float angle_radians = acos(dot_product / (magnitude1 * magnitude2));
-
-  
     float angle_degrees = angle_radians * 180.0 / PI;
 
     return angle_degrees;
